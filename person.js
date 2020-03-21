0f7696f6bd1c3539e5ba7fb4b83324a1cd0499fd
Belhoussine <54045588+Belhoussine@users.noreply.github.com>
2020-03-21 02:43:16 -0700

    Add files via upload

--- person.js
+++ person.js
@@ -1,77 +1,86 @@
 function person(x, y, id) {
   //ID and State
   this.id = id;
+  this.immunity = Math.random();
   this.infected = false;
   this.recovered = false;
-  this.moving = false;
+  this.hospitalized = false;
   this.dead = false;
+  this.moving = false;
+  
 
   //Position and Movement
   this.x = x;
   this.y = y;
-  this.speedx = Math.floor(random(-mvFreq, mvFreq)) / speed;
-  this.speedy = Math.floor(random(-mvFreq, mvFreq)) / speed;
+  this.speedx = (Math.floor(rand(-mvFreq, mvFreq+1)) / mvFreq) * speed;
+  this.speedy = (Math.floor(rand(-mvFreq, mvFreq+1)) / mvFreq) * speed;
 
   //Visual Characteristics
   this.healthyColor = 'rgba(20, 100, 20, 0.65)';
   this.infectedColor = 'rgba(100, 20, 20, 0.65)';
-  this.recoveredColor = 'rgba(20, 20, 100, 0.65)';
-  this.invertH =  'rgba(235, 155, 235, 0.65)';
-  this.invertI =  'rgba(155 , 235 , 235, 0.65)';
-  this.invertR = 'rgba(235, 235, 155, 0.65)';
+  this.recoveredColor = 'rgba(20, 20, 135, 0.7)';
+  this.deadColor = 'rgba(0, 0, 0, 0.70)';
+  this.hostpitalizedColor = 'rgba(20, 100, 100, 0.65)';
   this.size = personSize;
 
   //Setting Movement State (Moving or not)
-  if (this.speedx == 0 && this.speedy) {
-    this.moving = false;
-  } else {
+  if (this.speedx != 0 && this.speedy != 0) {
     this.moving = true;
   }
-
+  
   // Moving the person if it has a moving state
   this.move = function() {
-    if( ! this.dead){
+    if( !this.dead && this.moving){
       this.x += this.speedx;
       this.y += this.speedy;
       this.meetsPeople();
       this.hitsBorders();
-      this.display();
     }
+    this.display();
   }
   
     //Displaying the person
   this.display = function() {
-    noStroke();
+    if(this.moving || this.dead)
+      noStroke();
+    else if(!this.moving)
+      stroke(230,200,20);
     if (this.infected){
       fill(this.infectedColor);
-      if(night){
-        fill(this.invertI);
-      }
     }
     else if(this.recovered){
       fill(this.recoveredColor);
-      if(night){
-        fill(this.invertR);
-      }
     }
-    else{
-      fill(this.healthyColor)
-      if(night)
-        fill(this.invertH);
+    else if (this.dead){
+      fill(this.deadColor);
+    }else if(this.hospitalized){
+      fill(this.hospitalizedColor);
+    }else{
+      fill(this.healthyColor);
     }
     circle(this.x, this.y, this.size);
   }
 
   //Border Collision
   this.hitsBorders = function() {
-    //left and right
-    if (this.x - (this.size / 2) < 0 || this.x + (this.size / 2) > canvasWidth) {
-      this.speedx = -this.speedx;
+    //left 
+    if (this.x - (this.size / 2) <= 0 ) {
+      this.speedx = Math.abs(this.speedx);
       return true;
     }
-    //top and bottom
-    if (this.y - (this.size / 2) < 0 || this.y + (this.size / 2) > canvasHeight) {
-      this.speedy = -this.speedy;
+    //right
+    if (this.x + (this.size / 2) >= canvasWidth) {
+      this.speedx = -Math.abs(this.speedx);
+      return true;
+    }
+    //top 
+    if (this.y - (this.size / 2) <= 0 ) {
+      this.speedy = Math.abs(this.speedy);
+      return true;
+    }
+    //bottom
+    if (this.y + (this.size / 2) >= canvasHeight) {
+      this.speedy = -Math.abs(this.speedy);
       return true;
     }
     return false;
@@ -82,7 +91,7 @@ function person(x, y, id) {
     for (let i = 0; i < populationSize; i++) {
       if (i == this.id) continue;
       let other = population[i];
-      if (dist(other.x, other.y, this.x, this.y) <= personSize && other.infected && ! this.infected && ! this.recovered) {
+      if (this.gotInfected(other)) {
         this.infected = true;
         infected++;
         healthy --;
@@ -91,21 +100,68 @@ function person(x, y, id) {
     return false;
   }
   this.evolve = function(){
-      this.speedx = Math.floor(random(-speed, speed)) / speed;
-  this.speedy = Math.floor(random(-speed, speed)) / speed;
-    if(! this.dead){
-      if(this.infected && random(1) > recoveryRate * (1 - complicationRate) * 0.5){
-        this.infected = false;
-        this.recovered = true;
-        recovered++;
-        infected --;
+    if (!this.dead ){
+      if (this.infected){
+        let randomnessFactor = Math.random() * fRate * dayTime;
+        if(randomnessFactor <= 0.2){
+          if(this.immunity < deathRate){
+            this.die();
+          }else if(this.immunity < complicationRate){
+            this.getHospitalized();
+          }else{
+            this.recover();
+          }
+        }
       }
-      else if(this.infected && random(1) < deathRate / (1-complicationRate)){
-        this.infected = false;
-        this.dead = true;
-        dead++;
-        infected --;
-      }
+      if(!this.moving && Math.random() < 0.02)
+        this.startMoving();
+      else if(this.moving && Math.random() < 0.02)
+        this.stopMoving();
     }
+  }
+  
+  this.startMoving = function(){
+    this.speedx = this.newSpeed();
+    this.speedy = this.newSpeed();
+    this.moving = true;
+  }
+  
+  this.stopMoving = function(){
+    this.speedx = 0;
+    this.speedy = 0;
+    this.moving = false;
+  }
+  this.die = function(){
+    this.moving = false;
+    this.infected = false;
+    this.recovered = false;
+    this.dead = true;
+    infected--;
+    dead++;
+  }
+  
+  this.getHospitalized = function(){
+    this.hospitalized = true;
+    this.immunity/= 1.5;
+  }
+  
+  this.recover = function(){
+    recovered ++;
+    infected --;
+    this.recovered = true;
+    this.infected = false;
+    this.hospitalized = false;
+  }
+  
+  this.gotInfected = function(other){
+    if( ! this.moving ) console.log(touching(this, other))
+    return touching(this, other) && other.infected && !this.infected && ! this.recovered;
+  }
+  
+  this.quarantine = function(){
+    
+  }
+  this.newSpeed = function(){
+    return (Math.floor(rand(-mvFreq, mvFreq+1)) / mvFreq) * speed;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,35 +1,39 @@
 var population = [];
 
 //Sizes and Dimensions
-var populationSize = 400;
+var populationSize = 500;
 var personSize = 10;
 var canvasWidth = 750;
 var canvasHeight = 500;
 var offset = personSize / 2;
 var countryScale = 1000;
+var fRate = 40;
 
 // Time Tracking
 var startTime;
-var currentWeek = 1;
-var dayTime = 8;
-var nightTime = 2;
+var currentDay = 1;
+var dayTime = 1;
+var nightTime = 0;
 var night = false;
 
 // Population Characteristics
-var infected = 3;
+var infected = 5;
 var healthy = populationSize - infected;
 var recovered = 0;
 var dead = 0;
-var mvFreq = 4;
-var speed = 3;
+var mvFreq = 2;
+var speed = 2;
+var summarizing = false;
 
 // Virus Data
 var recoveryRate = 0.9;
 var deathRate = 0.1;
-var complicationRate = 0.2;
+var complicationRate = 0.3;
+
 
 // Initialization Function
 function setup() {
+  frameRate(fRate);
   canvasWidth = windowWidth;
   canvasHeight = windowHeight;
   createCanvas(canvasWidth, canvasHeight);
@@ -42,8 +46,9 @@ function setup() {
 function draw() {
   background(255);
   wakeUp();
-  updateText();
   sleep();
+  updateText();
+  //summary()
 }
 
 //Moving and Interacting with other people
@@ -51,6 +56,7 @@ function wakeUp() {
   night = false;
   for (let p = 0; p < populationSize; p++) {
     population[p].move();
+    population[p].evolve();
   }
 }
 
@@ -60,57 +66,51 @@ function sleep() {
   let currentTime = second();
   if ((startTime + dayTime) % 60 == currentTime) {
     startTime = currentTime;
-    night = true;
-    background(255);
-    updateText();
-    filter(INVERT);
-    evolve()
-    noLoop();
+    //night = true;
+    //background(255);
+    //updateText();
+    //filter(INVERT);
+    //evolve()
+    //noLoop();
     setTimeout(function() {
-      currentWeek++;
-      loop();
+      currentDay++;
+      //loop();
     }, nightTime * 1000);
   }
 }
 
-function evolve() {
-  night = false;
-  for (let p = 0; p < populationSize; p++) {
-    population[p].evolve();
-    population[p].move();
-  }
-}
-
 function updateText() {
+  noStroke();
   textSize(28);
   fill(0, 0, 0, 150);
-  text('Week ' + currentWeek, 10, 30);
+  text('Day ' + currentDay, 10, 30);
   textSize(15);
-  text('Initial Population: ' + populationSize +',000', 10, 60);
+  text('Initial Population: ' + populationSize, 10, 60);
   textSize(20);
-  fill('rgba(20, 100, 20, 0.65)');
-  text('Healthy: ' + healthy +',000', canvasWidth - 180, 30);
-  fill('rgba(100, 20, 20, 0.65)');
-  text('Infected: ' + infected +',000', canvasWidth - 180, 60);
-  fill('rgba(20, 20, 100, 0.65)');
-  text('Recovered: ' + recovered +',000', canvasWidth - 180, 90);
-  fill('rgba(0, 0, 0, 0.65)');
-  text('Dead: ' + dead +',000', canvasWidth - 180, 120);
+  fill('rgba(20, 100, 20, 0.7)');
+  text('Healthy: ' + healthy, canvasWidth - 140, 30);
+  fill('rgba(100, 20, 20, 0.7)');
+  text('Infected: ' + infected, canvasWidth - 140, 60);
+  fill('rgba(20, 20, 100, 0.7)');
+  text('Recovered: ' + recovered, canvasWidth - 140, 90);
+  fill('rgba(0, 0, 0, 0.7)');
+  text('Dead: ' + dead, canvasWidth - 140, 120);
 }
 
 //Creates a population
 function createPopulation() {
   for (let i = 0; i < populationSize; i++) {
-    let x = random(offset, canvasWidth - offset);
-    let y = random(offset, canvasHeight - offset);
+    let x = rand(offset, canvasWidth - offset);
+    let y = rand(offset, canvasHeight - offset);
+    let tempPerson = new person(x, y, i);
     let intersect = false;
     for (let j = 0; j < i; j++) {
-      let other = population[j];
-      if (dist(other.x, other.y, x, y) <= personSize + 5)
+      let existingPerson = population[j];
+      if (touching(tempPerson, existingPerson))
         intersect = true;
     }
     if (!intersect) {
-      population.push(new person(x, y, i));
+      population.push(tempPerson);
     } else {
       i--;
     }
@@ -122,4 +122,24 @@ function infectIndividuals() {
     population[i].infected = true;
   }
 }
-// ISOLATION OF INFECTED CASES
+
+function rand(max, min) {
+  return Math.random() * (max - min) + min;
+}
+
+function distSquared(x1, y1, x2, y2) {
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+  return dx * dx + dy * dy;
+}
+
+function touching(a, b) {
+  return distSquared(a.x, a.y, b.x, b.y) <= personSize * personSize;
+}
+
+function summary() {
+  if (currentDay % 7 == 0) {
+    background(255,255,255,25);
+  }
+}
+  // ISOLATION OF INFECTED CASES
